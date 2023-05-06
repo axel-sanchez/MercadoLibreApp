a84f534a5b7c4b963bdbf0f5e940eb7abd6e063f
Axel <axelsan97@hotmail.com>
2023-05-06 19:06:52 -0300

    cree la segunda screen que muestra los productos, aun tiene algunos bugs

--- app/src/main/java/com/example/mercadolibreapp/presentation/SearchScreen.kt
+++ app/src/main/java/com/example/mercadolibreapp/presentation/SearchScreen.kt
@@ -1,10 +1,13 @@
 package com.example.mercadolibreapp.presentation
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,13 +15,17 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.rememberImagePainter
 import com.example.mercadolibreapp.R
 import com.example.mercadolibreapp.data.models.DataProducts
+import com.example.mercadolibreapp.helpers.Constants.ApiError.*
 import com.example.mercadolibreapp.presentation.viewmodel.SearchViewModel
 
 /**
@@ -29,60 +36,154 @@ import com.example.mercadolibreapp.presentation.viewmodel.SearchViewModel
 fun SearchScreen(
     query: String, viewModel: SearchViewModel
 ) {
-    val products: DataProducts by viewModel.getProductLiveData().observeAsState(initial = DataProducts(listOf()))
-    
-    /*val showLoading = rememberSaveable(inputs = , stateSaver = ) {
-
-    }*/
+    viewModel.getProduct(query)
+    val dataProducts: DataProducts by viewModel.getProductLiveData()
+        .observeAsState(initial = DataProducts())
 
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .background(colorResource(id = R.color.colorPrimary))
     ) {
         val (emptyState, loading) = createRefs()
 
-        EmptyState(query = query, modifier = Modifier.constrainAs(emptyState) {
+        EmptyState(modifier = Modifier.constrainAs(emptyState) {
             top.linkTo(parent.top)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
-        })
+        }, dataProducts)
 
-        Loading(modifier = Modifier.constrainAs(loading){
+        Loading(modifier = Modifier.constrainAs(loading) {
             top.linkTo(parent.top)
             bottom.linkTo(parent.bottom)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
-        })
+        }, dataProducts)
+
+        ProductList(dataProducts)
     }
 }
 
 @Composable
-fun Loading(modifier: Modifier){
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressIndicator()
+fun Loading(modifier: Modifier, dataProducts: DataProducts) {
+    if (dataProducts.products == null) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
     }
 }
 
 @Composable
-fun EmptyState(query: String, modifier: Modifier) {
+fun EmptyState(modifier: Modifier, dataProducts: DataProducts) {
+    dataProducts.products?.let { products ->
+        if (products.isEmpty()) {
+            ErrorCard(EMPTY_PRODUCTS.error, modifier)
+        }
+    } ?: run {
+        dataProducts.apiError?.let{
+            ErrorCard(it.error, modifier)
+        }
+    }
+}
+
+@Composable
+fun ErrorCard(message: String, modifier: Modifier) {
     Card(
         modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
             .fillMaxWidth()
             .padding(12.dp),
-        elevation = 4.dp
+        elevation = 4.dp,
     ) {
         Text(
-            text = "No se encontraron productos vinculados a su búsqueda: $query",
+            text = message,
             Modifier
                 .fillMaxWidth()
                 .padding(10.dp),
             softWrap = true,
             style = TextStyle(fontSize = 18.sp)
         )
+    }
+}
+
+@Composable
+fun ProductList(dataProducts: DataProducts) {
+    if (!dataProducts.products.isNullOrEmpty()) {
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            itemsIndexed(dataProducts.products) { index, product ->
+                ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+                    val (card, title, price, freeShipping, divider) = createRefs()
+                    Card(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .height(150.dp)
+                            .width(150.dp)
+                            .padding(10.dp)
+                            .constrainAs(card) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(title.start)
+                            }
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(
+                                data = product?.thumbnail
+                            ),
+                            contentDescription = "imagen del producto en el item"
+                        )
+                    }
+                    Text(
+                        modifier = Modifier
+                            .constrainAs(title) {
+                                top.linkTo(card.top)
+                                start.linkTo(card.end)
+                                end.linkTo(parent.end)
+                                width = Dimension.fillToConstraints
+                            }
+                            .padding(top = 10.dp, end = 10.dp),
+                        text = product?.title ?: "",
+                        softWrap = true
+                    )
+                    Text(
+                        modifier = Modifier
+                            .constrainAs(price) {
+                                top.linkTo(title.bottom)
+                                start.linkTo(card.end)
+                                end.linkTo(parent.end)
+                                width = Dimension.fillToConstraints
+                            }
+                            .padding(top = 10.dp, end = 10.dp),
+                        text = product?.price.toString(),
+                        softWrap = true
+                    )
+                    product?.shipping?.freeShipping?.let { isFreeShipping ->
+                        if (isFreeShipping) {
+                            Text(
+                                modifier = Modifier
+                                    .constrainAs(freeShipping) {
+                                        top.linkTo(price.bottom)
+                                        start.linkTo(card.end)
+                                        end.linkTo(parent.end)
+                                        width = Dimension.fillToConstraints
+                                    }
+                                    .padding(top = 10.dp, end = 10.dp, bottom = 10.dp),
+                                text = stringResource(R.string.envio_gratis),
+                                softWrap = true, color = Color.Green
+                            )
+                        }
+                    }
+                    if (index != dataProducts.products.size - 1) {
+                        Divider(
+                            modifier = Modifier.constrainAs(divider) {
+                                top.linkTo(card.bottom)
+                            },
+                            color = Color.Gray,
+                            thickness = 1.dp
+                        )
+                    }
+                }
+            }
+        }
     }
 }
